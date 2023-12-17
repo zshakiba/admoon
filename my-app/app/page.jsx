e52f0ec8b9a87f01f80295ab68a66d8afe40f75a
zshakiba <zeinabshakiba@gmail.com>
2023-12-17 11:13:25 +0330

    fix

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md">
+        <h1 className="text-4xl font-bold mb-4">Welcome to My Website</h1>
+        <p className="text-lg text-gray-700">
+          Explore and discover amazing content on our platform. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
